f9d964221f66cbb7fe64b51030adeae722592961
andresc1125 <andresc1125@gmail.com>
2014-10-29 10:05:21 -0500

    added support for base model in Green Core

---
+++ core/src/com/ahcobos/greencore/gcmodel/GCBaseModel.java
@@ -0,0 +1,105 @@
+package com.ahcobos.greencore.gcmodel;
+
+import java.util.HashMap;
+
+import com.ahcobos.greencore.gcstate.GCState;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+
+public class GCBaseModel extends GCModel {
+
+	public GCBaseModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Sprite getSprite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, GCState> getStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addState(GCState state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCurrentState(String key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setCurrentState(GCState state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GCState getCurrentState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getOriginX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getOriginY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getRotation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getScaleX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getScaleY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ core/src/com/ahcobos/greencore/gcmodel/GCModel.java
@@ -0,0 +1,25 @@
+package com.ahcobos.greencore.gcmodel;
+
+import java.util.HashMap;
+
+import com.ahcobos.greencore.gcstate.GCState;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+
+public abstract class GCModel {
+	public abstract Sprite getSprite();
+	public abstract HashMap<String, GCState> getStates();	
+	public abstract void addState(GCState state);
+	public abstract void setCurrentState(String key);
+	public abstract void setCurrentState(GCState state);
+	public abstract GCState getCurrentState();
+	public abstract float getX();
+	public abstract float getY();
+	public abstract float getOriginX();
+	public abstract float getOriginY();
+	public abstract float getWidth();
+	public abstract float getHeight();
+	public abstract float getRotation();
+	public abstract float getScaleX();
+	public abstract float getScaleY();
+}
