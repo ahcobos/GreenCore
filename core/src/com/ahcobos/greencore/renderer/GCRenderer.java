59d7635ae4319022d04a9862cfc05407a197cd17
andresc1125 <andresc1125@gmail.com>
2014-11-02 21:52:26 -0500

    added abstract class for renderer

---
+++ core/src/com/ahcobos/greencore/renderer/GCRenderer.java
@@ -0,0 +1,13 @@
+package com.ahcobos.greencore.renderer;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * @author ahcobos
+ * @since 2014-11-02 21:49:01
+ */
+public abstract class GCRenderer {
+	public abstract void render(); 
+}
