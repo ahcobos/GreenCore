e3e33d83bb214fff55512f2680b6edac7c3d6d14
andresc1125 <andresc1125@gmail.com>
2014-12-13 21:16:36 -0500

    updated examples in order to test the new graphics gestros

--- core/src/com/ahcobos/examples/staticelements/StaticElements.java
+++ core/src/com/ahcobos/examples/staticelements/StaticElements.java
@@ -2,10 +2,14 @@ package com.ahcobos.examples.staticelements;
 
 import com.ahcobos.examples.staticelements.screens.FirstScreen;
 import com.ahcobos.greencore.assests.AssetsLoaderModel;
+import com.ahcobos.greencore.inputprocesors.GCGestureListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
 
 public class StaticElements extends Game {
 
@@ -14,8 +18,11 @@ public class StaticElements extends Game {
 	@Override
 	public void create() {
 		new AssetsLoaderModel();
+		TextureAtlas atlas;
 		AssetsLoaderModel.addTexture("badlogic.jpg", "base_image");
+		AssetsLoaderModel.addPack("and_engine.pack", "and_engine");
 		this.mScreen = new FirstScreen();
+		Gdx.input.setInputProcessor(new GestureDetector(new GCGestureListener()));
 		this.setScreen(mScreen);
 	}
 
--- core/src/com/ahcobos/examples/staticelements/elements/FaceSmileState.java
+++ core/src/com/ahcobos/examples/staticelements/elements/FaceSmileState.java
@@ -1,12 +1,17 @@
 package com.ahcobos.examples.staticelements.elements;
 
 import com.ahcobos.greencore.gcstate.GCBaseState;
+import com.ahcobos.greencore.sourcemanager.GCOpenTexturePackerSource;
+import com.ahcobos.greencore.sourcemanager.GCTextureSource;
 import com.badlogic.gdx.math.Rectangle;
 
 public class FaceSmileState extends GCBaseState {
 	
 	public FaceSmileState() {
 		this.setTextureHash("base_image");
+		this.setGraphicsSourceManager(new GCOpenTexturePackerSource("and_engine", "tank"));
+		//GCTextureSource nSource = new GCTextureSource("base_image");
+		//this.setGraphicsSourceManager(nSource);
 		this.setBoundsOnTexture(new Rectangle(0,0,50,50));
 	}
 
