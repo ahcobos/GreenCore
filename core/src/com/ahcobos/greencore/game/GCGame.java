305822598b43b1749caf2688fedb05db37188ec4
andresc1125 <ahcobos@yahoo.com>
2015-04-05 00:34:28 -0500

    setScreen sets the Gesture listener Too

--- core/src/com/ahcobos/greencore/game/GCGame.java
+++ core/src/com/ahcobos/greencore/game/GCGame.java
@@ -1,12 +1,24 @@
 package com.ahcobos.greencore.game;
 
+import com.ahcobos.greencore.inputprocesors.GCGestureListener;
+import com.ahcobos.greencore.layer.GCBaseScreen;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.input.GestureDetector;
 
 public class GCGame extends Game {
 
 	@Override
 	public void create() {
 
+	}
+	
+	@Override
+	public void setScreen (Screen screen) 
+	{
+		super.setScreen(screen);
+		Gdx.input.setInputProcessor(new GestureDetector(new GCGestureListener((GCBaseScreen) screen)));
 	}
 
 }
