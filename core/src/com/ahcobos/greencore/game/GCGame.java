2f0ef432a888940a0d6081b94c00ab4f394efc44
andresc1125 <ahcobos@yahoo.com>
2015-04-19 18:55:11 -0500

    added hashmap of screens to GCGame

--- core/src/com/ahcobos/greencore/game/GCGame.java
+++ core/src/com/ahcobos/greencore/game/GCGame.java
@@ -1,24 +1,50 @@
 package com.ahcobos.greencore.game;
 
+import java.util.HashMap;
+
 import com.ahcobos.greencore.inputprocesors.GCGestureListener;
 import com.ahcobos.greencore.layer.GCBaseScreen;
+import com.ahcobos.greencore.layer.GCScreen;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.input.GestureDetector;
 
 public class GCGame extends Game {
+	
+	protected HashMap<String, GCScreen> screens;
+
+	//=================================
+	// Inherit Methods
+	//=================================
 
 	@Override
 	public void create() {
-
+		
 	}
 	
-	@Override
-	public void setScreen (Screen screen) 
+	public void setScreen (String key) 
 	{
-		super.setScreen(screen);
-		Gdx.input.setInputProcessor(new GestureDetector(new GCGestureListener((GCBaseScreen) screen)));
+		super.setScreen(this.getScreens().get(key));
+		Gdx.input.setInputProcessor(new GestureDetector(new GCGestureListener((GCBaseScreen) this.getScreens().get(key))));
 	}
-
+	
+	public HashMap<String, GCScreen> getScreens()
+	{
+		if(this.screens == null)
+		{
+			this.screens = new HashMap<String, GCScreen>();
+		}
+		return this.screens;
+	}
+	
+	//=================================
+	// Methods
+	//=================================
+	
+	public void addScreen(String key, GCScreen screen)
+	{
+		if(!this.getScreens().containsKey(key))
+			screens.put(key, screen);
+	}
 }
